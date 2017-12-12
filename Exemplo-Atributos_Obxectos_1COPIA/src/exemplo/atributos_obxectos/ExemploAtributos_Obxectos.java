a85c199349b320d41828942b7dd426e07c02a77d
dfernandezguerreiro <dfernandezguerreiro@victoria2.danielcastelao.org>
2017-12-12 09:16:49 +0100

    COMMIT GITHUB.

---
+++ Exemplo-Atributos_Obxectos_1COPIA/src/exemplo/atributos_obxectos/ExemploAtributos_Obxectos.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemplo.atributos_obxectos;
+
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+public class ExemploAtributos_Obxectos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Conta obxConta=new Conta("millonetis", "1111111", 100000000); //Siempre en orden Nombre, DNI y saldo.
+        System.out.println(obxConta.toString()); // <- eso, coge el valor de arriba, con el toString de Conta.java
+        
+        Conta obxConta2=new Conta("pobrillo", "12121212", 200);
+        System.out.println(obxConta2.toString());
+        
+        
+    }
+    
+}
